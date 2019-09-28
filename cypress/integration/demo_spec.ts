cef3077648724e4306bb3bcd5ca6d4e3b6037bad
ohbarye <over.rye@gmail.com>
2019-09-28 23:32:57 +0900

    Test for page transition

--- cypress/integration/demo_spec.ts
+++ cypress/integration/demo_spec.ts
@@ -2,3 +2,9 @@ it("my first test", () => {
   cy.visit("https://ohbarye.hatenablog.jp/");
   cy.contains("SoundCloud").should("exist");
 });
+
+it("goes to next page", () => {
+  cy.visit("https://ohbarye.hatenablog.jp/");
+  cy.contains("次のページ").click();
+  cy.url().should("include", "https://ohbarye.hatenablog.jp/?page=");
+});
