75825728318b1c47f99df5c39055991b6f1b5324
ohbarye <over.rye@gmail.com>
2019-09-28 23:33:42 +0900

    Use beforeEach to tidy up tests

--- cypress/integration/demo_spec.ts
+++ cypress/integration/demo_spec.ts
@@ -1,10 +1,12 @@
-it("my first test", () => {
+beforeEach(() => {
   cy.visit("https://ohbarye.hatenablog.jp/");
+});
+
+it("my first test", () => {
   cy.contains("SoundCloud").should("exist");
 });
 
 it("goes to next page", () => {
-  cy.visit("https://ohbarye.hatenablog.jp/");
   cy.contains("次のページ").click();
   cy.url().should("include", "https://ohbarye.hatenablog.jp/?page=");
 });
